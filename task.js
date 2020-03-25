30447616e8c9e5a9ba731991efbb4b20a30ff690
Aderonke Odushola <aderonkediana@gmail.com>
2020-03-25 01:32:58 +0000

    iteration file

---
+++ task.js
@@ -0,0 +1,17 @@
+//this stores my name
+let name = 'Aderonke';
+
+//this is an array of my courses in HNG
+let course = ['frontend', 'design', 'coding'];
+
+//this displays my name and courses in HNG
+console.log(`My name is ${name} and my courses are ${course}.`);
+
+//this loops through my courses
+for (let i = 0; i<=200; i++){
+  if (i % 2 !== 0)
+  console.log(i);
+}
+
+
+
